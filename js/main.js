a58fd69a6639feada94a60730e9d2fb18e64a301
dsypien <davidsypien@gmail.com>
2013-09-17 01:00:05 -0400

    Add colors enum

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+var colors = {
+	"red":1, 
+	"orange":2,
+	"yellow":3,
+	"green":4,
+	"blue":5,
+	"purple":6,
+	"black":7,
+	"white":8,
+	"brown":9
+};
+
 $(document).ready(function(){
 	$('#main').animate({
 		opacity: 1.0,
 	  }, 1500 );
 	  
 	$('.smalltile').click(function(){
-		alert( this.id + " clicked");
+		alert( this.attributes.color.nodeValue + " clicked");
 	});
 });
