37343ddf34d57394c477760c317fb9581fdadeca
dsypien <davidsypien@gmail.com>
2013-09-19 17:42:55 -0400

    Add previous view button

--- js/main.js
+++ js/main.js
@@ -12,22 +12,47 @@ var colors = {
 
 var viewstack = new Array();
 
+function getCurrentView(){
+	var size = viewstack.length;
+	
+	if( size < 1){
+		return null;
+	}
+	
+	return viewstack[ size - 1];
+}
+
 $(document).ready(function(){
 	$('#main').animate({
-		opacity: 1.0,
+		opacity: 1.0
 	  }, 1500 );
 	  
 	viewstack.push("#mainview");
+	 
+	$('#previousview').click(function(){
+		hideview(viewstack.pop());
+		
+		$(getCurrentView()).animate({
+			"height": "90%"
+			}, 1000, function(){
+				//oncomplete
+			}
+		);
+	});	
 	  
 	$('.smalltile').click(function(){
 		$('#tileview').css("background", this.attributes.color.nodeValue);
+		hideview('#mainview');
+		
 		viewstack.push("#tileview");
-	
-		$('#mainview').animate({
+	});
+});
+
+function hideview(viewname){
+	$(viewname).animate({
 			"height": "0"
 			}, 1000, function(){
 			    // on complete
 			}
-		);	
-	});
-});
+		);
+}
