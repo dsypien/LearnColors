9d2b74b71f185000debf6072ee9a0477b9817952
dsypien <davidsypien@gmail.com>
2013-10-31 22:04:56 -0400

    refactoring/organzing

--- js/main.js
+++ js/main.js
@@ -1,84 +1,5 @@
 var selectedColor = null;
-var colors = {
-    "red": {
-        "images": ["images/red/apple"
-                    , "images/red/cherry"
-                    , "images/red/pepper"
-                    , "images/red/raspberry"
-                    , "images/red/tomato"
-                    , "images/red/tomatoe"
-                    , "images/red/watermellon"
-        ]
-    },
-    "orange": {
-        "images": ["images/orange/apricot"
-                    , "images/orange/carrots"
-                    , "images/orange/orange"
-                    , "images/orange/pie"
-        ]
-    },
-    "yellow": {
-        "images": ["images/yellow/banana"
-                    , "images/yellow/bee"
-                    , "images/yellow/cheese"
-                    , "images/yellow/cheese2"
-                    , "images/yellow/fish"
-                    , "images/yellow/lemon"
-                    , "images/yellow/sun"
-        ]
-    },
-    "green": {
-        "images": ["images/green/candy"
-                    , "images/green/fish"
-                    , "images/green/frog"
-                    , "images/green/leaf"
-                    , "images/green/olive"
-                    , "images/green/pear"
-                    , "images/green/snake"
-                    , "images/green/soybeans"
-                    , "images/green/turtle"
-        ]
-    },
-    "blue": {
-        "images": ["images/blue/blue"
-                    , "images/blue/elephant"
-                    , "images/blue/bird"
-                    , "images/blue/butterfly"
-        ]
-    },
-    "purple": {
-        "images": ["images/purple/grapes"
-                    , "images/purple/ice-cream"
-                    , "images/purple/onion"
-                    , "images/purple/star"
-        ]
-    },
-    "black": {
-        "images": ["images/black/camera"
-                    , "images/black/cat"
-                    , "images/black/computer"
-                    , "images/black/microphone"
-                    , "images/black/tv"
-                    , "images/black/tv2"
-        ]
-    },
-    "pink": {
-        "images": ["images/pink/flower"
-                    , "images/pink/pencil"
-                    , "images/pink/pinkbow"
-                    , "images/pink/rabbit"
-        ]
-    },
-    "brown": {
-        "images": ["images/brown/cake"
-                    , "images/brown/chocolate"
-                    , "images/brown/cookie"
-                    , "images/brown/dog"
-                    , "images/brown/hamster"
-                    , "images/brown/tedy"
-        ]
-    }
-};
+var colors = new Colors();
 
 var viewstack = new Array();
 var imageToCutout = new Array();
@@ -135,7 +56,7 @@ $(document).ready(function () {
 
         // Set the background of each cutout image object
         for (i = 0; i < 4; i++) {
-            $('#cutoutimage' + i).css("background-image", "url(" + colors[selectedColor].images[i] + "_deColorD.png)");
+            $('#cutoutimage' + i).css("background-image", "url(" + colors.data[selectedColor].images[i] + "_deColorD.png)");
             $('#cutoutimage' + i).attr("num", i)
         }
 
@@ -143,7 +64,7 @@ $(document).ready(function () {
         var imageAry = new Array();
         for (i = 0; i < 4; i++) {
             imageAry[i] = {
-                "filename": colors[selectedColor].images[i] ,
+                "filename": colors.data[selectedColor].images[i] ,
                 "index" : i
             };
         }
@@ -173,9 +94,10 @@ $(document).ready(function () {
 
                         var cutoutindex = imageToCutout[ui.draggable.attr("num")];
                         console.log(cutoutindex);
+
                         $('#cutoutimage' + cutoutindex).css("opacity", "0");
-                        $('#cutoutimage' +cutoutindex)
-                            .css("background-image", "url(" + colors[selectedColor].images[num] + ".png)")
+                        $('#cutoutimage' + cutoutindex)
+                            .css("background-image", "url(" + colors.data[selectedColor].images[num] + ".png)")
                             .animate({ opacity: 1 });
                     });
                 }
