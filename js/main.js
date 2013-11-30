b298dc5ca2adee86aeb6519bb0c39c3e54ec3999
dsypien <davidsypien@gmail.com>
2013-11-30 14:43:41 -0500

    hide text on back of card before flipping it back

--- js/main.js
+++ js/main.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
                 }
                 else
                 {
+                   $('.back').html();
                    $('#flipcard').removeClass('flipped');
                    $('.face').removeClass(flipCardColor +'tile');
 
