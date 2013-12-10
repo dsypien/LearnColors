163009608526a73ac469866481ddc71c84737072
dsypien <davidsypien@gmail.com>
2013-12-10 08:30:35 -0500

    Seperate drop handle functionality into its own method

--- js/main.js
+++ js/main.js
@@ -76,30 +76,6 @@ function onPreviousClick(){
     }   
 }
 
-function generateImageTiles() {
-    // Clear all images in tile column2
-    $('#tilecolumn2').html("");
-
-    for (i = 0; i < 4; i++) {
-        $('<div/>', {
-            id :'imagetile' + i,
-            "class": "imagetile",
-            "num": i
-        }).appendTo('#tilecolumn2');
-    }
-
-    $('.imagetile').draggable({
-        containment: "window",
-        revert: function (event, ui) {
-            $(this).data("uiDraggable").originalPosition = {
-                top: 0,
-                left: 0
-            }
-            return !event;
-        }
-    });
-}
-
 function onCellClick(){
     var tileLabelString = capitalizeFirstChar(selectedColor);
     $('#bigtilelabel').html(tileLabelString);
@@ -140,29 +116,7 @@ function onCellClick(){
         imageToCutout[obj[0].index]= i;
     }
 
-    for (i = 0; i < 4; i++) {
-        var obj = $.grep(imageAry, function(e){ return e.filename ==  imageAry[i].filename; })
-
-        $('#cutoutimage' + obj[0].index).droppable({
-            accept: "#imagetile" + i ,
-            drop: function (event, ui) {
-                $(ui.draggable).animate({
-                    opacity: 0
-                }, 300, function () {
-                    var num = parseInt(ui.draggable.attr("num"));
-
-                    var cutoutindex = parseInt(imageToCutout.indexOf(num));
-                    console.log("num: "+ num + "  cutoutindex: " + cutoutindex);
-
-                    $('#cutoutimage' + cutoutindex).css("opacity", "0");
-
-                    $('#cutoutimage' + cutoutindex)
-                        .css("background-image", "url(" + colors.data[selectedColor].images[cutoutindex] + ".png)")
-                        .animate({ opacity: 1 });
-                });
-            }
-        });
-    }
+    addDropEventHandlers(imageAry);
      
     viewstack.push("#mainview");
     hideview('#mainview');
--- js/matchgame.js
+++ js/matchgame.js
@@ -23,3 +23,29 @@ function generateImageTiles() {
         }
     });
 }
+
+function addDropEventHandlers(imageAry){
+        for (i = 0; i < 4; i++) {
+        var obj = $.grep(imageAry, function(e){ return e.filename ==  imageAry[i].filename; })
+
+        $('#cutoutimage' + obj[0].index).droppable({
+            accept: "#imagetile" + i ,
+            drop: function (event, ui) {
+                $(ui.draggable).animate({
+                    opacity: 0
+                }, 300, function () {
+                    var num = parseInt(ui.draggable.attr("num"));
+
+                    var cutoutindex = parseInt(imageToCutout.indexOf(num));
+                    console.log("num: "+ num + "  cutoutindex: " + cutoutindex);
+
+                    $('#cutoutimage' + cutoutindex).css("opacity", "0");
+
+                    $('#cutoutimage' + cutoutindex)
+                        .css("background-image", "url(" + colors.data[selectedColor].images[cutoutindex] + ".png)")
+                        .animate({ opacity: 1 });
+                });
+            }
+        });
+    }
+}
