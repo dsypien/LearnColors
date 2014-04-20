48ded83daca438feb8bd1b2bcee052538e44c57b
David Sypien <sypiend@mskcc.org>
2014-04-20 15:08:15 -0400

    fix some lint warnings

--- js/colors.js
+++ js/colors.js
@@ -1,81 +1,39 @@
 function Colors(){
     this.data= {
         "red": {
-            "images": ["images/red/apple"
-                        , "images/red/cherry"
-                        , "images/red/pepper"
-                        , "images/red/raspberry"
-                        , "images/red/tomato"
-                        , "images/red/tomatoe"
-                        , "images/red/watermelon"
+            "images": ["images/red/apple", "images/red/cherry", "images/red/pepper", "images/red/raspberry", "images/red/tomato", "images/red/tomatoe", "images/red/watermelon"
             ]
         },
         "orange": {
-            "images": ["images/orange/apricot"
-                        , "images/orange/carrots"
-                        , "images/orange/orange"
-                        , "images/orange/pie"
+            "images": ["images/orange/apricot", "images/orange/carrots", "images/orange/orange", "images/orange/pie"
             ]
         },
         "yellow": {
-            "images": ["images/yellow/banana"
-                        , "images/yellow/bee"
-                        , "images/yellow/cheese"
-                        , "images/yellow/cheese2"
-                        , "images/yellow/fish"
-                        , "images/yellow/lemon"
-                        , "images/yellow/sun"
+            "images": ["images/yellow/banana", "images/yellow/bee", "images/yellow/cheese", "images/yellow/cheese2", "images/yellow/fish", "images/yellow/lemon", "images/yellow/sun"
             ]
         },
         "green": {
-            "images": ["images/green/candy"
-                        , "images/green/fish"
-                        , "images/green/frog"
-                        , "images/green/leaf"
-                        , "images/green/olive"
-                        , "images/green/pear"
-                        , "images/green/snake"
-                        , "images/green/soybeans"
-                        , "images/green/turtle"
+            "images": ["images/green/candy", "images/green/fish", "images/green/frog", "images/green/leaf", "images/green/olive", "images/green/pear", "images/green/snake", "images/green/soybeans", "images/green/turtle"
             ]
         },
         "blue": {
-            "images": ["images/blue/blue"
-                        , "images/blue/elephant"
-                        , "images/blue/bird"
-                        , "images/blue/butterfly"
+            "images": ["images/blue/blue", "images/blue/elephant", "images/blue/bird", "images/blue/butterfly"
             ]
         },
         "purple": {
-            "images": ["images/purple/grapes"
-                        , "images/purple/ice-cream"
-                        , "images/purple/onion"
-                        , "images/purple/star"
+            "images": ["images/purple/grapes", "images/purple/ice-cream", "images/purple/onion", "images/purple/star"
             ]
         },
         "black": {
-            "images": ["images/black/camera"
-                        , "images/black/cat"
-                        , "images/black/computer"
-                        , "images/black/microphone"
-                        , "images/black/tv"
-                        , "images/black/tv2"
+            "images": ["images/black/camera", "images/black/cat", "images/black/computer", "images/black/microphone", "images/black/tv", "images/black/tv2"
             ]
         },
         "pink": {
-            "images": ["images/pink/flower"
-                        , "images/pink/pencil"
-                        , "images/pink/pinkbow"
-                        , "images/pink/rabbit"
+            "images": ["images/pink/flower", "images/pink/pencil", "images/pink/pinkbow", "images/pink/rabbit"
             ]
         },
         "brown": {
-            "images": ["images/brown/cake"
-                        , "images/brown/chocolate"
-                        , "images/brown/cookie"
-                        , "images/brown/dog"
-                        , "images/brown/hamster"
-                        , "images/brown/tedy"
+            "images": ["images/brown/cake", "images/brown/chocolate", "images/brown/cookie", "images/brown/dog", "images/brown/hamster", "images/brown/tedy"
             ]
         }
     }
