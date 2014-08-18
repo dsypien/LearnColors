85c52b7c2ff0a05adfe1b5af87e4dbb85b1d8b49
David Sypien <davidsypien@gmail.com>
2014-08-17 20:21:30 -0400

    remove audio object cache

--- js/audioplayer.js
+++ js/audioplayer.js
@@ -2,30 +2,13 @@ var EmmasApp = EmmasApp || {};
 
 EmmasApp.audioPlayer = (function(){
 	var soundFile,
-		audio,
-		introAudio, //= new Audio(getFilePath("xylophone")), 
-		applauseAudio,
-		minionAudio,
-		soundObject = {};
+		audio;
 
 	function play(fileName){
-		// Play the cached version if found in the soundObject
-		// if(fileName in soundObject){
-		// 	playCached(soundObject[fileName]);
-		// 	return;
-		// }
-
 		soundFile = getFilePath(fileName);
         audio = new Audio(soundFile);
         audio.play();
-
-        // store the audio in the sound object
-        soundObject[fileName] = audio;
 	}
-
-	// function playCached(audioObj){		
-	// 	audioObj.play();
-	// }
 
 	function getFilePath(name){
 		return '../audio/' + name + '.wav';
