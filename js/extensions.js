a197bcb158d9076e70d84bbfed1b7d5f3f7c7c9a
dsypien <davidsypien@gmail.com>
2014-12-26 17:44:19 -0500

    Remove font adjustment extension

--- js/extensions.js
+++ js/extensions.js
@@ -10,31 +10,3 @@ Array.prototype.shuffle =  function() {
 
     return this;
 };
-
-// //Jquery Extensions
-// jQuery.fn.extend({
-//   adjustFont: function(isBig) {
-//     var width = this.width();
-//     var height = this.height();
-//     var fontsize;
-
-//     if(isBig === undefined || isBig === false){
-//         if((width / 3) < height){
-//             fontsize = width / 20;
-//         }
-//         else{
-//             fontsize =  height /10;
-//         }
-//     }
-//     else{
-//      if((width / 3) < height){
-//             fontsize = width / 2;
-//         }
-//         else{
-//             fontsize =  height /1.5;
-//         }   
-//     }
-
-//     this.css("font-size", fontsize );
-//   }
-// });
